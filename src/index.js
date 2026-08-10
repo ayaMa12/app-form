92882c19dea512f7af7be854b556280aa00dbc36
Aya Mahmoud <aya122972@gmail.com>
2026-08-10 20:11:23 +0300

    return routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
- const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* 🔴 قمنا فقط بإضافة basename="/app-form" داخل القوس هنا */}
-    <HashRouter basename="/app-form">
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
