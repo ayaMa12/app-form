3b3e533153eb030fec1411bfbe0be272a0368fdd
Aya Mahmoud <aya122972@gmail.com>
2026-08-10 20:03:46 +0300

    fix routing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
  const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    {/* 🔴 قمنا فقط بإضافة basename="/app-form" داخل القوس هنا */}
+    <HashRouter basename="/app-form">
       <App />
     </HashRouter>
   </React.StrictMode>
